b17b7371a0257798eaf1311a86714308c05b1b14
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-20 19:14:01 -0500

    adds option to use default formatting

--- resize.sh
+++ resize.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 # A simple bash script that uses the sips library to convert images for web
 
-# copy folder of images
+# Run the script with a positional argument to prefix the batch resized files
 
 echo "Naming all files with the prefix ${1}"
+read -p "Do you want to use the default format options? \ To format images at 70% quality, press ENTER. \ To format at a specified quality, enter a number between 1 and 100 " formatOpt
+
+echo "THIS IS THE VARIABLE ${formatOpt}"
+if ($formatOpt = ""); then 
+    formatOpt = 70
+else
+    regex ='^[0-9]+$'
+    if ! [[ formatOpt =~ $regex ]]; then
+        formatOpt = formatOpt
+    else
+       read -p "${format} is not a valid formatting option. To proceed with the default options, press ENTER."
+       formatOpt = 70
+    fi
+fi
+
+if [!formatOptions]; then 
+    echo "Could not format image. Goodbye";done
+
 for i in *.*;
-    do sips -s format jpeg -s formatOptions 70 "${i}" --out "${1}-${i}.jpg"
+    do sips -s format jpeg -s formatOptions formatOpt  "${i}" --out "${1}-${i}.jpg"
     echo "Converting ${i} to a compressed jpeg";done
