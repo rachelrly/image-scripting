f5d5d7f277ecd064abbab593313757aec4323709
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-21 13:28:59 -0500

    adds idempotent test script and comments out option input

--- resize.sh
+++ resize.sh
@@ -3,25 +3,30 @@
 
 # Run the script with a positional argument to prefix the batch resized files
 
+#if prefix
 echo "Naming all files with the prefix ${1}"
-read -p "Do you want to use the default format options? \ To format images at 70% quality, press ENTER. \ To format at a specified quality, enter a number between 1 and 100 " formatOpt
+# read -p "Do you want to use the default format options?
 
-echo "THIS IS THE VARIABLE ${formatOpt}"
-if ($formatOpt = ""); then 
-    formatOpt = 70
-else
-    regex ='^[0-9]+$'
-    if ! [[ formatOpt =~ $regex ]]; then
-        formatOpt = formatOpt
-    else
-       read -p "${format} is not a valid formatting option. To proceed with the default options, press ENTER."
-       formatOpt = 70
-    fi
-fi
+# To format images at 70% quality, press ENTER. 
 
-if [!formatOptions]; then 
-    echo "Could not format image. Goodbye";done
+# To format at a specified quality, enter a number between 1 and 100 " format
+
+# echo "THIS IS THE VARIABLE ${formatOpt}"
+# if ($format = ""); then 
+#     formatOpt = 70
+# else
+#     regex ='^[0-9]+$'
+#     if ! [[ $formatOpt =~ $regex ]]; then
+#         formatOpt = formatOpt
+#     else
+#        read -p "${format} is not a valid formatting option. To proceed with the default options, press ENTER."
+#        formatOpt = 70
+#     fi
+# fi
+
+# if [!formatOpt]; then 
+#     echo "Could not format image. Goodbye";done
 
 for i in *.*;
-    do sips -s format jpeg -s formatOptions formatOpt  "${i}" --out "${1}-${i}.jpg"
+    do sips -s format jpeg -s formatOptions 30  "${i}" --out "${1}-${i%png}jpg"
     echo "Converting ${i} to a compressed jpeg";done
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run this file with `bash test.sh` to test the other scripts on sample images
+# This idempotent test is meant to be run from the root directory
+
+rm -rf test1 # delete test folder if already present
+cp -a test-images test1
+cd test1 
+bash ../resize.sh test1
+cd ..
+echo "Created test images in /test1"
