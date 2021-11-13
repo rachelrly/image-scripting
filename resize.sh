e0049d32e1878d2d70b6ead3149858205bf57c2b
Rachel Reilly <r.a.reill18@gmail.com>
2021-11-13 15:32:16 -0600

    makes output more legible with color line breaks and feedback

--- resize.sh
+++ resize.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 function resize {
+    BLUE='\033[1;34m'
+    CYAN='\033[0;36m'
+    RED='\033[1;31m'
+    NC='\033[0m' # no color
+
     img_file=$1
     img=${img_file##*/}
 
     ext="${img##*.}"
     name="${img%.*}"
 
-    echo "Converting $img from $ext to compressed jpeg"
+    echo -e "\n $BLUE Converting $img from $ext to compressed jpeg... $NC"
 
     longestSide=500
     quality=high
@@ -17,10 +22,10 @@ function resize {
     sips -s format jpeg -s formatOptions $quality -Z $longestSide "$img" --out $new_img
 
     if test -f $new_img; then 
-        echo "Successfully converted from $img to $new_img"  
+        echo -e "\n $CYAN Successfully converted from $img to $new_img $NC"  
         ls -l $new_img
     else 
-        echo "Conversion of $img failed"
+        echo -e "$RED Conversion of $img failed $NC"
     fi
 }
 
--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-test_dir = "test"
+CYAN='\033[0;36m'
+RED='\033[1;31m'
+NC='\033[0m' # no color
+
+test_dir="test"
 
 rm -rf $test_dir 
 cp -a test-images $test_dir
@@ -8,4 +12,6 @@ cd $test_dir
 bash ../resize.sh 
 cd ..
 
-echo "Created test images in /$test_dir"
+
+echo -e "\n $CYAN Created test images in /$test_dir $NC"
+ls -l $test_dir
